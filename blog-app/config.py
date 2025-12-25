b5712a39d14879c92babd4ef941811140e508414
sehisto-art <sehisto@gmail.com>
2025-12-25 12:53:27 +0300

    Fix formatting issues in config.py

--- blog-app/config.py
+++ blog-app/config.py
@@ -1,9 +1,8 @@
-import os\
-\
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))\
-\
-class Config:\
-    SECRET_KEY = "change_me_in_production"\
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "instance", "blog.db")\
-    SQLALCHEMY_TRACK_MODIFICATIONS = False\
-}
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = "change_me_in_production"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "instance", "blog.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
